53353c4a23bfc5eb7eb73fba2e09510cbf797877
HUDSON <hudsoncleiton01@gmail.com>
2026-01-05 13:37:11 -0300

    wip: removendo comentarios do crud de motos

--- app/api/dados.ts
+++ app/api/dados.ts
@@ -1,17 +1,12 @@
-// define o tipo de dados do estado da aplicação. fabricas do objeto com id e idCounter,
-// propriedade somente leitura que gera o proximo id
 type StateType = {
   fabricante: { id: number; fabricante: string; cilindrada: number }[];
   idCounter: number;
 };
-// exteção do objeto global (globalThis) para armazenar o estado das motos.
-// Isso permite que o estado persista entre diferentes requisições durante o runtime da aplicação
-// (util em desenvolvimento e ambiente serverless)
+
 const globalForState = globalThis as unknown as {
   fabricanteState?: StateType;
 };
-// inicialização do estado global, verifica se o estado ja existe no objeto global.
-// se não existir, cria um novo estado com os dados iniciais.
+
 if (!globalForState.fabricanteState) {
   globalForState.fabricanteState = {
     fabricante: [
@@ -19,9 +14,6 @@ if (!globalForState.fabricanteState) {
       { id: 2, fabricante: "Yamaha", cilindrada: 600 },
       { id: 3, fabricante: "Kawasaki", cilindrada: 600 },
     ],
-    // getter que calcula dinamicamente o proximo id disponivel
-    // se existirem motos: retorna o maior id atual +1
-    // se não existir motos retorna 1
     get idCounter() {
       return this.fabricante.length > 0
         ? Math.max(...this.fabricante.map((m) => m.id)) + 1
@@ -29,8 +21,6 @@ if (!globalForState.fabricanteState) {
     },
   };
 }
-// exporta a referencia ao estado global para ser usado nos endpoints
-// todos os endpoits compartilham a mesma instancia do estado
 const state = globalForState.fabricanteState;
 
 export default state;
